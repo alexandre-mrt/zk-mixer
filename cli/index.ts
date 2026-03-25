a101cefea69c42cd65fa390e26f8cc6968a6bbd6
Alexandre Mourot <Alex.mourot@icloud.com>
2026-03-25 01:35:50 +0100

    feat(cli): add watch command for real-time event monitoring

--- cli/index.ts
+++ cli/index.ts
@@ -2,6 +2,7 @@ import { Command } from "commander";
 import { depositCommand } from "./deposit";
 import { withdrawCommand } from "./withdraw";
 import { statusCommand } from "./status";
+import { watchCommand } from "./watch";
 
 const program = new Command();
 
@@ -14,5 +15,6 @@ program
 program.addCommand(depositCommand);
 program.addCommand(withdrawCommand);
 program.addCommand(statusCommand);
+program.addCommand(watchCommand);
 
 program.parse();
